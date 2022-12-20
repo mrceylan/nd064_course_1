2874e9fdd6d88c61f166c00d0c7fc2b364a02a7a
Emre Ceylan <emreceylan89@gmail.com>
2022-12-20 22:05:29 +0100

    project push

--- project/techtrends/app.py
+++ project/techtrends/app.py
@@ -2,12 +2,22 @@ import sqlite3
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
+import logging
+
+TOTAL_CONNECTION_COUNT = 0
+
+logging.basicConfig(
+                level=logging.DEBUG, format='%(levelname)s: %(name)s %(asctime)s %(message)s')
 
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global TOTAL_CONNECTION_COUNT
+
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+
+    TOTAL_CONNECTION_COUNT += 1
     return connection
 
 # Function to get a post using its ID
@@ -36,13 +46,16 @@ def index():
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info("A non-existing article is accessed and a 404 page is returned.")
       return render_template('404.html'), 404
     else:
+      app.logger.info("Article \"%s\" retrieved!", post['title'])
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info("The \"About Us\" page is retrieved.")
     return render_template('about.html')
 
 # Define the post creation functionality 
@@ -60,10 +73,30 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
-
+            app.logger.info("A new article \"%s\" is created!", title)
             return redirect(url_for('index'))
 
     return render_template('create.html')
+
+# Define the healthz endpoint
+@app.route('/healthz')
+def healthz():
+    data = {}
+    app.logger.info("healthzzzzz")
+    data['result'] = 'OK - healthy'
+    return data
+
+# Define the metrics endpoint
+@app.route('/metrics')
+def metrics():
+    global TOTAL_CONNECTION_COUNT
+    connection = get_db_connection()
+    postCount = connection.execute('SELECT COUNT(*) FROM posts').fetchone()[0]
+    connection.close()
+    data = {}
+    data['db_connection_count'] = TOTAL_CONNECTION_COUNT
+    data['post_count'] = postCount
+    return data
 
 # start the application on port 3111
 if __name__ == "__main__":
